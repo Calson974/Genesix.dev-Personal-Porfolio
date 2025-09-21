800a90691f671abec4e4e2053ad9749e8251fcd7
Calson974 <94759948+Calson974@users.noreply.github.com>
2025-09-20 20:56:30 -0700

    Remove duplicate createRoot call

--- src/main.tsx
+++ src/main.tsx
@@ -13,8 +13,6 @@ if (import.meta.env.DEV) {
   })
 }
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-)
 ReactDOM.createRoot(document.getElementById('app')!).render(
   <React.StrictMode>
     <HelmetProvider>
